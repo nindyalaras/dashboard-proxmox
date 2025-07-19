a6f6ee9b9865669a8b64f4dba3e80a2471d3345e
nindyalaras <nindyadwilarasati09@gmail.com>
2025-07-20 03:22:06 +0700

    Update Log.js

--- server/models/Log.js
+++ server/models/Log.js
@@ -4,9 +4,9 @@ const LogSchema = new mongoose.Schema({
   action: String,
   vm_id: String,
   status: String,
-  ip_address: String, // tambahkan ini
+  ip_address: String, 
   timestamp: { type: Date, default: Date.now },
-  message: String     // opsional (buat keterangan umum)
+  message: String     
 });
 
 module.exports = mongoose.model('Log', LogSchema);
